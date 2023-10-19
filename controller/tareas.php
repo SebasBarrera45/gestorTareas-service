12cf83da155b6f898885ab55e403d7c498a0d6a2
SebasBarrera45 <l.bafs020307@itses.edu.mx>
2023-10-19 13:27:54 -0600

    Actualizacion

--- controller/tareas.php
+++ controller/tareas.php
@@ -10,83 +10,30 @@ $body = json_decode(file_get_contents("php://input"), true);
 // Determinar la opción proporcionada en la solicitud GET
 switch ($_GET["option"]) {
 
-    case "login";
-        // Llamar al método "login" en la instancia de Usuario con los datos del cuerpo
-        $datos = $modelos->login($body['correo'], $body['pass']);
-        // Devolver los datos como respuesta en formato JSON
+    case "agregarTarea":
+        $datos = $modelos->agregarTarea($body['titulo'], $body['descripcion'], $body['fecha']);
         echo json_encode($datos);
         break;
 
-    case "agregarUsuario";
-        // Llamar al método "agregarUsuario" en la instancia de Usuario con los datos del cuerpo
-        $datos = $modelos->agregarUsuario($body['nombre'], $body['apellidoP'], $body['apellidoM'], $body['correo'], $body['pass']);
-        // Devolver los datos como respuesta en formato JSON
+    case "traerTarea":
+        $datos = $modelos->traerTarea();
         echo json_encode($datos);
         break;
 
-    case "traerMascotas";
-        // Llamar al método "get_mascotas" en la instancia de Usuario
-        $datos = $modelos->get_mascotas();
-        // Devolver los datos como respuesta en formato JSON
+    case "traerTarea_x_id":
+        $datos = $modelos->traerTarea_x_id($body["id"]);
         echo json_encode($datos);
         break;
 
-    case "get_pass";
-        // Llamar al método "get_pass" en la instancia de Usuario con el correo del cuerpo
-        $datos = $modelos->get_pass($body['correo']);
-        // Devolver los datos como respuesta en formato JSON
+    case "editarTarea":
+        $datos = $modelos->editarTarea($body['id'], $body['titulo'], $body['descripcion'], $body['fecha']);
         echo json_encode($datos);
         break;
 
-    case "get_alumno_x_id";
-        // Llamar al método "get_alumno_x_id" en la instancia de Usuario con el ID de mascotas del cuerpo
-        $datos = $modelos->get_alumno_x_id($body["idmascotas"]);
-        // Devolver los datos como respuesta en formato JSON
+    case "deleteTarea":
+        $datos = $modelos->deleteTarea($body['id']);
         echo json_encode($datos);
         break;
 
-    case "traerMascota";
-        // Llamar al método "get_mascota" en la instancia de Usuario
-        $datos = $modelos->get_mascota();
-        // Devolver los datos como respuesta en formato JSON
-        echo json_encode($datos);
-        break;
-
-    case "traerUsuarios";
-        // Llamar al método "traerUsuarios" en la instancia de Usuario
-        $datos = $modelos->traerUsuarios();
-        // Devolver los datos como respuesta en formato JSON
-        echo json_encode($datos);
-        break;
-
-    case "agregarMascota";
-        // Llamar al método "agregarMascota" en la instancia de Usuario con los datos del cuerpo
-        $datos = $modelos->agregarMascota($body['nombre'], $body['edad'], $body['causa']);
-        // Devolver los datos como respuesta en formato JSON
-        echo json_encode($datos);
-        break;
-
-    case "editarActividad";
-        // Llamar al método "editarActividad" en la instancia de Usuario con los datos del cuerpo
-        $datos = $modelos->editarActividad($body['idmascotas'], $body['nombre'], $body['edad'], $body['causa']);
-        // Devolver los datos como respuesta en formato JSON
-        echo json_encode($datos);
-        break;
-
-    case "get_actividad_x_id";
-        // Llamar al método "get_actividad_x_id" en la instancia de Usuario con el ID de mascotas del cuerpo
-        $datos = $modelos->get_actividad_x_id($body['idmascotas']);
-        // Devolver los datos como respuesta en formato JSON
-        echo json_encode($datos);
-        break;
-
-    case "deleteActividad";
-        // Llamar al método "deleteActividad" en la instancia de Usuario con el ID de mascotas del cuerpo
-        $datos = $modelos->deleteActividad($body['idmascotas']);
-        // Devolver los datos como respuesta en formato JSON
-        echo json_encode($datos);
-        break;
-
-    // Las opciones comentadas no están actualmente en uso y pueden eliminarse o descomentarse según sea necesario.
 }
 ?>
